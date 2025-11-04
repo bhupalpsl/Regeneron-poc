9fdb359a1bc7dfee5869056ec90d6fd19ffe743d
bhupalpsl <bhupal_pamuluri@persistent.com>
2025-11-04 15:54:49 +0530

    first commit

---
+++ Regeneron-poc-main/modules/jenkins_instance/user-data.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+apt update -y
+apt install -y openjdk-11-jdk wget gnupg
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt update -y
+apt install -y jenkins
+systemctl enable jenkins
+systemctl start jenkins
+
+#Docker
+
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl status docker --no-pager
+sudo usermod -aG docker jenkins
+docker --version
+sudo docker run hello-world
